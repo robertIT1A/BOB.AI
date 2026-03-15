1cb9a5606dca0c4f8fad8ffc79818bd97dfd346e
robertIT1A <robertoguevarrajr25@gmail.com>
2026-03-15 20:07:02 +0800

    Refactor BOB_AI chatbot implementation

--- BOB_AI.py
+++ BOB_AI.py
@@ -5,7 +5,7 @@ import random
 
 
 
-# run di na natapos
+# run di na natapos , di padin tapos
 
 
 open = input("Type START to start talking to our friendly AI chat bot named BOB_AI, Type QUIT to close the program >> ")
